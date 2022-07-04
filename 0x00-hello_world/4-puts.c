c7bc536736d8bd30a8e1a89ccfb55c2bc27dc21b
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-04 14:12:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+/**
+* main - to print the c program exaclty,
+* Return: 0 if exited properly
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
