769ce364e6e1d39a45dccb1195ee24b74bea79ff
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 13:10:48 -0700

    print3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
-*main -> assign a random number to the variable n each each time
-*it is executed and print the last digit of the number in the variable 
-*Return: always 0
+* main -> assign a random number to the variable n each each
+* time it is executed and print the last digit of the number
+* in the variable 
+* Return: always 0
 */
 
 int main(void)
@@ -17,7 +19,7 @@ for (ch = 97; ch <= 122; ch++)
 
 for (ch = 65; ch <= 90; ch++)
 {
-        putchar(ch);
+	putchar(ch);
 }
 
 putchar(10);/* this is an ascii code for new line*/
