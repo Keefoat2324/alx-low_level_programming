ed29c6bc10ff9f13d106b8f05dd27e5f953c9f9c
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 13:34:57 -0700

    print4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main -> assign a random number to the variable n each each time
-*it is executed and print the last digit of the number in the variable 
-*Return: always 0
+* main -> assign a random number to the variable n each each time
+* it is executed and print 
+* the last digit of the number in the variable 
+* Return: always 0
 */
 
 int main(void)
