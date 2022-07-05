6698cd720bdfebe39435891b3bdd6c240a4ee705
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 15:46:05 -0700

    print101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main -> assign a random number to the variable n each each time
-*it is executed and print the last digit of the number in the variable 
-*Return: always 0
+* main - assign a random number to the variable n each time
+* it is executed and print
+* last digit a new line, to the standard error.
+* Return: Always 0 (Success)
 */
 
 int main(void)
@@ -12,35 +13,25 @@ int main(void)
 int ch, n, m;
 for (ch = 48; ch <= 57 ; ch++)
 {
-        for (n = 49; n <= 57; n++)
-        {
-            	for (m = 50; m<= 57; m++)
-
+	for (n = 49; n <= 57; n++)
+	{
+		for (m = 50; m <= 57; m++)
 		{
-
 			if (n > ch  && m > n)
-
-                {
-
-                        putchar(ch);
-                        putchar(n);
+		{
+			putchar(ch);
+			putchar(n);
 			putchar(m);
-                        if (ch != 55 ||  n != 56 || n != 57)
-
-                        {
-                                putchar(44);
-                                putchar(32);
-                         }
-
-                }
-
-
+			if (ch != 55 ||  n != 56 || m != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 
-        }
+		}
+	}
 }
-	
-
 putchar(10);
 
 return (0);
