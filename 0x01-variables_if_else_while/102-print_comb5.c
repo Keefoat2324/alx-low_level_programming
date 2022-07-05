6e0f48e4a7b3a0e9f7ecfa4360ac986b028685d1
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 16:11:45 -0700

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,22 @@ int main(void)
 {
 int ch1, ch2;
 
-for (ch1 = 0; ch1 <= 98; ch1++)
+for (ch1 = 0; ch1 < 100; ch1++)
 {
-	for (ch2 = ch1 + 1; ch2 <= 99; ch2++)
+	for (ch2 = 0; ch2 < 100; ch2++)
 	{
-		putchar(ch1 / 10 + '0');
-		putchar(ch1 % 10 + '0');
-		putchar(32);
-		putchar(ch2 / 10 + '0');
-		putcha(ch2 % 10 +  '0');
-		if (ch1 / 10 != 9 || ch1 % 10 != 8)
+		if (ch1 < ch2)
 		{
-			putchar(44);
+			putchar(ch1 / 10 + '0');
+			putchar(ch1 % 10 + '0');
 			putchar(32);
+			putchar(ch2 / 10 + '0');
+			putcha(ch2 % 10 +  '0');
+				if (ch1 / 10 != 9 || ch1 % 10 != 8)
+				{
+					putchar(44);
+					putchar(32);
+				}
 		}
 	}
 }
