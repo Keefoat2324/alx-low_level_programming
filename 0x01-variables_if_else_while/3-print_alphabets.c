ffaf16bd6fb804360345e499e8f8452f961a819d
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 13:15:24 -0700

    print3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
-* main -> assign a random number to the variable n each each
-* time it is executed and print the last digit of the number in the variable 
+* main -> assign a random number to the variable n each each time it is 
+* executed and print the last digit of the number in the variable 
 * Return: always 0
 */
 
