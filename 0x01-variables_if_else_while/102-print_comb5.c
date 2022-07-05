d24d15c941c8f1394e169b1846b2325d47093dc9
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 16:00:49 -0700

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,10 +17,10 @@ for (ch1 = 0; ch1 <= 98; ch1++)
 	for (ch2 = ch1 + 1; ch2 <= 99; ch2++)
 	{
 		putchar(ch1 / 10 + '0');
-		putchar(ch1 % 10 + '0);
+		putchar(ch1 % 10 + '0');
 		putchar(32);
 		putchar(ch2 / 10 + '0');
-		putcha(ch2 % 10 +  '0);
+		putcha(ch2 % 10 +  '0');
 		if (ch1 / 10 != 9 || ch1 % 10 != 8)
 		{
 			putchar(44);
