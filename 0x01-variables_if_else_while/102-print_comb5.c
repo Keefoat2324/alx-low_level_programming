d8748d0be8404a7f4ec2d36fb10a8dc449bfe0cf
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 15:58:51 -0700

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,41 +2,32 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main -> assign a random number to the variable n each each time
-*it is executed and print the last digit of the number in the variable 
-*Return: always 0
+* main - assign a random number to the variable n each time
+* it is executed and print
+* last digit a new line, to the standard error.
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
-int ch1;
-int ch2;
+int ch1, ch2;
 
-for (ch1 = 0; ch1 < 100; ch1++)
+for (ch1 = 0; ch1 <= 98; ch1++)
 {
-	for (ch2 = 0; ch2 < 100; ch2++)
+	for (ch2 = ch1 + 1; ch2 <= 99; ch2++)
 	{
-
-		if (ch1 < ch2)
+		putchar(ch1 / 10 + '0');
+		putchar(ch1 % 10 + '0);
+		putchar(32);
+		putchar(ch2 / 10 + '0');
+		putcha(ch2 % 10 +  '0);
+		if (ch1 / 10 != 9 || ch1 % 10 != 8)
 		{
-			putchar(ch1 / 10 + '0');
-			putchar(ch1 % 10 + '0);
+			putchar(44);
 			putchar(32);
-			putchar(ch2 / 10 + '0');
-			putcha(ch2 % 10 +  '0);
-				if ((ch1 +100) + ch2 != 9899)
-				{
-					putchar(44);
-					putchar(32);
-
-				}
 		}
-
-
-
 	}
 }
-
 putchar(10);/* this is an ascii code for new line*/
 
 return (0);
