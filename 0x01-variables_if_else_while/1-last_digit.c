55337f270124e75979cd7102b4d6c746790b0037
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-05 15:05:23 -0700

    mash

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,21 +11,24 @@
 int main(void)
 {
 int n;
+int lastd;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+lastd = n % 10;
+
 printf("Last digit of %d is ", n);
 if (n > 5)
 {
-	printf("%d and is greater than 5", n);
+	printf("%d and is greater than 5", lastd);
 }
 if (n == 0)
 {
-	printf("%d and is 0", n);
+	printf("%d and is 0", lastd);
 }
 if (n < 6 && n != 0)
 {
-	printf("%d and is less than 6 and not 0", n);
+	printf("%d and is less than 6 and not 0", lastd);
 }
 printf("\n");
 
