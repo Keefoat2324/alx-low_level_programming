5f0bae6739e0d90d26a92ce27650830d08bc1adb
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-07 03:21:50 -0700

    lowercasecharacter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _islower - to print lowercase alphabet
+* Return: 1 and 0 depending
+*/
+
+int _islower(int c) /* refer int c*/
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/* refer int c*/
+		if (c == i)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0)
+		}
+	}
+
+}
