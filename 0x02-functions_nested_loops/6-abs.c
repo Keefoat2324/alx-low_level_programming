f904bfb2457010544d2ee129962703755124733b
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-07 12:43:44 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _abs - to compute an absolute value of an integer
+* @x: the argument value it received
+* Return: 1 and 0 depending on the conditions
+*/
+
+int _abs(int x)
+{
+	return (int abs(int x));
+}
+
