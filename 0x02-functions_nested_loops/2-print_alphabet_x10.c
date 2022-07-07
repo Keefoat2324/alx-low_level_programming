d750e4fa374fe330acc2c9555044511dce875ab5
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-06 17:17:17 -0700

    10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - to print 10x lowercase alphabet
+*/
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 11; j++)
+	{
+		int i;
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
