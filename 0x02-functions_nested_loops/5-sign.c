3001e8d86eeb2b66e2fe25ec67b1ee0ff27f2de6
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-07 12:09:49 -0700

    sign5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,23 +6,27 @@
 * Return: 1 and 0 depending on the conditions
 */
 
-int n;
-
 int print_sign(int n)
 {
+	int n;
+
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
+	_putchar('\n');
 	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
+	_putchar('\n');
 	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
+	_putchar('\n');
 }
+
