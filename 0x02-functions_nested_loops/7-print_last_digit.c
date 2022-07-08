4bba695b731e7426c364be1ebe4a5eba0b121c1c
“Owolabi <owolabitaofeekabidemi@yahoo.com>
2022-07-07 22:01:34 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_last_digit - to compute an absolute value of an integer
+* @x: the argument value it received
+* Return: the last digit
+*/
+
+int print_last_digit(int x)
+{
+	int n;
+
+	if (x < 0)
+		x = -x;
+	n = x % 10;
+	_putchar( n + '0')
+	return (n);
+}
